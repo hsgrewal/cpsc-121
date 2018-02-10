9767f81bdcd4b9f7e480bc883c1bb47dde0b5742
Harkishan Singh Grewal <harkishangrewal@csu.fullerton.edu>
2018-02-10 11:03:18 -0800

    Add final project

---
+++ project/README.md
@@ -0,0 +1,172 @@
+# Final Project
+
+## Harkishan Singh Grewal
+
+CPSC-121 - Programming Concepts
+March 20, 2015
+Written in C++
+
+## Simple Blackjack Game
+The project is a C++ application of a simple blackjack game. The subsections
+below describe the following:
+  1. Card and Ticket Number
+  2. Point System
+  3. Stage of Game
+  4. Dealer and Player
+  5. Constant
+
+### Card and Ticket Number
+This C++ program shuffles 4 decks of standard 52 cards. A standard deck of 52
+cards has 4 suits: spade, heart, diamond and club. Each suit contains 13
+cards: Ace, 2, 3, 4, 5, 6, 7, 8, 9, 10, Jack, Queen, and King. For the
+purposes of this program, it is assumed that Ace is 0, Jack is 10, Queen is
+11, and King is 12. In the other words, a number is assigned to each face card
+and making the 13 cards: 0 (Ace), 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 (Jack),
+11 (Queen), and 12 (King). The decks are numbered as 0, 1, 2, and 3 for the
+first, second, third and fourth deck respectively. The suits (spade, heart,
+diamond, and club) are numbered as 0, 1, 2, and 3 respectively. Each card
+from the four decks is associated with an unique ticket number. For the first
+deck, the 13 spades from Ace to King have ticket numbers 0, 1, 2, 3, 4, ...
+11, and 12 respectively. The 13 hearts from Ace to King in the first deck have
+ticket numbers 13, 14, 15, ... 24, and 25 respectively. The 13 diamonds from
+Ace to King in the first deck have ticket numbers 26, 27, 28, ... 37, and 38.
+The 13 clubs from Ace to King in the first deck have ticket numbers 39, 40,
+41, ... 50, and 51. For the 52 cards in the second deck, the numbering
+continues from the first deck. The 13 spades from Ace to King in the second
+deck have ticket numbers 52, 53, 54, ... 63, and 64. The rest of cards in the
+remaining decks are numbered in the same manner. Using the ticket number
+enables the program to identify the deck number, suit, and the real card
+number. For example, 12 is the king of spade from the first deck, 60 is the
+number 9 of spade from the second deck and 200 is the number 6 of club from
+the fourth deck.
+
+In terms of implementation, the program uses 1D array to hold all the ticket
+numbers. Since there are 208 cards in 4 decks, the integer array of 208
+elements can hold the ticket numbers from 0 to 207, as the numbering scheme
+described above.
+
+### Point System
+The point system specifies the points associated with each card regardless
+its suit. This is used when counting the total points of a given hand or
+combination of cards. The total points is the summation of points associated
+with each card. The point system is given below. For example, if the player
+has the hand: 3 of Spade, 4 of Club and 5 of heart, then the player has a
+total of 12 points. For another example, the player has Ace of Club and Queen
+of Heart at hand. The player would have 11 or 21 points.
+
+  > **Point System Associated with Each Card**
+  >   Card        Point
+  >     Ace         1 or 11
+  >     2           2
+  >     3           3
+  >     4           4
+  >     5           5
+  >     6           6
+  >     7           7
+  >     8           8
+  >     9           9
+  >     10          10
+  >     Jack        10
+  >     Queen       10
+  >     King        10
+
+### Stage of Game
+The simple blackjack game has the following stages from the start to end of game:
+
+  * Stage 1
+    The stage 1 initializes the environment of each game. The game environment
+    includes 4 decks of cards, players, and dealer. Players can be friends
+    while the game creator is the dealer. Or the program can randomly pick one
+    person as the dealer. The environment setup is described below:
+    1. The four decks of cards are created and shuffled
+    2. The information of 7 players are read from the file
+    3. The information of 7 dealers are read from the file
+    4. The number of players in one game depends on how many players would
+      like to join the game. Once the number of players is specified in the
+      beginning of the game, no additional player can join until the next
+      game. Once the player join the game, they will stay until the end of
+      game.
+    5. Once the number of players are set, the program randomly selects the
+      players from the player list
+    6. A dealer is randomly selected from the dealer list
+
+  * Stage 2
+    The dealer initializes the game by dealing cards to each player. The way
+    to deal cards in the first round is specified below:
+    1. The dealer deals two cards to each player. For example, the dealer
+      deals first two cards to the first player, and next two cards to the
+      second player until every player gets their first two cards. Please note
+      that the order of players to received their two cards depends on how
+      players were randomly selected from the player list.
+    2. After every player has two cards, the dealer deals one card to
+      him/her-self.
+
+  * Stage 3
+    The dealer starts to deal cards to each player and lastly him/her-self.
+    1. Before dealing a card to each player, the dealer will ask the player
+      the following question:
+        - Hit?
+        - Pass?
+    2. If the player chooses “Hit”, the dealer will deal one card to the
+      player. This process continues until the same player chooses “Pass” or
+      the player exceeds 21 (bust).
+    3. Once the dealer finishes dealing cards to one player, he/she will move
+      onto the next player.
+    4. The dealer finishes dealing cards to all players based on the one of
+      the following conditions:
+        - All players pass.
+        - Some players bust while the rest pass.
+    5. After the dealer finishes handling all players, he/she will start to
+      deal cards to him/her-self until one of the following conditions:
+        - The dealer exceeds 17 points.
+        - The dealer passes.
+    6. After the dealer finishes dealing cards to him/her-self, the stage is
+      terminated.
+
+  * Stage 4
+    It is time to count the points for each player and dealer using the point
+    system outlined above.
+
+  * Stage 5
+    The dealer (casino) starts to pay or collect the money.
+    1. As long as a player busts (even if the dealer busts), the dealer
+      collects a 10-dollar chip from the player.
+    2. If a player and the dealer do not bust and the player has fewer points
+      than the dealer, the dealer collects a 10-dollar chip from the player.
+    3. If a player does not bust but the dealer busts, the dealer pays a
+      10-dollar chip to the player.
+    4. If a player and the dealer do not bust and the player has more points
+      than the dealer, the dealer pays a 10-dollar chip to the player.
+    5. If a player and the dealer do not bust and the player and dealer have
+      the same number of points, no transaction occurs.
+
+### Dealer and Player
+In the player file, ”player.txt”, the first column is ID, second column is
+first name, and third column is the number of 10-dollar chips, owned by a
+player. In the dealer file, ”dealer.txt”, the first column is ID, second
+column is casino name, and the third column is the number of 10-dollar chips
+in a casino’s bank. The set of ticket numbers and player’s or dealer’s status
+will be updated while playing the blackjack. A dealer and player have the
+following data members in a C++ struct.
+  * ID: identification number.
+  * Name: first name.
+  * Chip: the number of 10-dollar chip.
+  * Ticket number: a set of ticket numbers possessed at the current game.
+  * Status (isWithdrawn): If the player has over 21 points, the player busts
+    and is withdrawn from the game. Or he/she can continue playing the game.
+
+Here is the C++ struct to represent both dealer and players:
+  > struct person {
+  >   int id;
+  >   std::string first_name;
+  >   int ticket_number[18];
+  >   bool isWithdrawn;
+  > };
+
+### Constants
+The constants are listed below:
+  1. The number of decks is 4.
+  2. The number of suits is 4.
+  3. The number of cards is 13.
+  4. The number of swaps is 1000.
+  5. Each chip has a value of 10 dollars.
---
+++ project/simpleBlackjackGame.cpp
@@ -0,0 +1,432 @@
+/*
+ * This program simulates a simple Blackjack game.
+ *
+ * Harkishan Singh Grewal
+ * April 1, 2015
+ * Due April 7, 2015
+ */
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+// Delcare constants.
+const int NUM_DECKS = 4;
+const int NUM_SUITS = 4;
+const int NUM_CARDS = 13;
+const int NUM_SWAPS = 1000;
+const int VALUE_CHIP = 10;
+
+// Declare struct.
+struct person{
+  int id;
+  string name;
+  int numChips;
+  int cards[18];
+  int counter = 0;
+  int score = 0;
+  bool isWithdrawn = false;
+};
+
+// Declare function prototypes.
+int swap(int* element1, int* element2);
+int shuffle(int card[], int nSwap);
+void determineCardInformation(int tickerNumber, int* cardNumber);
+void shufflePlayers(person players[], int nSwap);
+int determinePoints(int cardNumber, int score);
+int determineCardNumber(int ticketNumber);
+void displayCard(int cardNumber);
+
+int main(){
+  // Delcare main variables.
+  int deck[NUM_CARDS*NUM_DECKS*NUM_SUITS];
+  int numPlayers;
+  ifstream inputFile;
+  person players[7]; // Declare array of players.
+  person dealers[7]; // Declare array of players.
+  person dealer;
+  string hitOrPass;
+  string playAgain = "yes";
+
+  // Populate the ticketNumbers array.
+  for (int i = 0; i < NUM_CARDS*NUM_DECKS*NUM_SUITS; i++)
+    deck[i] = i;
+
+  cout << "Welcome to BLACKJACK!\n";
+  do{
+    int deckIndex = 0;
+    // Stage 1.
+    // Shuffle the cards.
+    shuffle(deck, NUM_SWAPS);
+
+    // Get the number of players.
+    cout << "\nEnter number of players: ";
+    cin >> numPlayers;
+    while (numPlayers<0 || numPlayers>7){ // validate user input.
+      cout << "Enter number of players: ";
+      cin >> numPlayers;
+    }
+
+    // Open file.
+    inputFile.open("player.txt");
+
+    // Check if file opened.
+    if (inputFile){
+      // Read numbers from file into the array.
+      for (int i = 0; i < 7; i++){
+        inputFile >> players[i].id;
+        inputFile >> players[i].name;
+        inputFile >> players[i].numChips;
+      }
+
+      // Close file after array is initialized.
+      inputFile.close();
+    }
+    else
+      cout << "Error opening the file.\n";
+
+    // Shuffle players.
+    shufflePlayers(players, NUM_SWAPS);
+
+    // Display player information.
+    for (int i = 0; i < numPlayers; i++){
+      cout << "\nPlayer " << i + 1 << ":\n"
+        << "   Id: " << players[i].id << endl
+        << "   Name: " << players[i].name << endl
+        << "   Chips: " << players[i].numChips << " ($"
+        << players[i].numChips*VALUE_CHIP << ")\n";
+    }
+
+    // Open file.
+    inputFile.open("dealer.txt");
+
+    // Check if file opened.
+    if (inputFile){
+      // Read numbers from file into the array.
+      for (int i = 0; i < 7; i++){
+        inputFile >> dealers[i].id;
+        inputFile >> dealers[i].name;
+        inputFile >> dealers[i].numChips;
+      }
+
+      // Close file after array is initialized.
+      inputFile.close();
+    }
+    else
+      cout << "Error opening the file.\n";
+
+    // select a random dealer for the game.
+    dealer = dealers[rand() % 7];
+
+    // Display dealer information.
+    cout << "\nDealer:\n"<< "   Id: " << dealer.id << endl
+      << "   Name: " << dealer.name << endl
+      << "   Chips: " << dealer.numChips << " ($"
+      << dealer.numChips*VALUE_CHIP << ")\n";
+
+    // Stage 2.
+    // deal 2 cards to each player.
+    while (deckIndex < numPlayers * 2){
+      for (int j = 0; j < numPlayers; j++){
+        for (int k = 0; k < 2; k++){ // 2 cards
+          // get the card number of the given card in the deck.
+          players[j].cards[players[j].counter] =
+            determineCardNumber(deck[deckIndex]);
+          // get the update the score with the points of dealt card.
+          players[j].score +=
+          determinePoints(players[j].cards[players[j].counter],
+            players[j].score);
+          // update indexes.
+          deckIndex++;
+          players[j].counter++;
+        }
+      }
+
+      // get the card number of the given card in the deck.
+      dealer.cards[dealer.counter] = determineCardNumber(deck[deckIndex]);
+      // get the update the score with the points of dealt card.
+      dealer.score += determinePoints(dealer.cards[dealer.counter],
+        dealer.score);
+      // update indexes.
+      deckIndex++;
+      dealer.counter++;
+    }
+
+    // Stage 3.
+    for (int j = 0; j < numPlayers; j++){
+      // display the cards that the player already has.
+      cout << "\nPlayer " << j + 1 << ":\n   Cards: ";
+      for (int i = 0; i < players[j].counter; i++){
+        if (i == players[j].counter - 1){
+          displayCard(players[j].cards[i]);
+          cout << endl;
+        }
+        else{
+          displayCard(players[j].cards[i]);
+          cout << ", ";
+        }
+      }
+      // display user score.
+      cout << "   Score: " << players[j].score << endl;
+      if (players[j].score < 21){
+        do{
+          // ask the player to hit or stay.
+          cout << "   Hit or Pass? ";
+          cin >> hitOrPass;
+
+          // update player info if player withdrew.
+          if (hitOrPass == "pass")
+            players[j].isWithdrawn = true;
+          else{
+            // deal another card to the player.
+            players[j].cards[players[j].counter] =
+              determineCardNumber(deck[deckIndex]);
+            // update player's score.
+            players[j].score +=
+              determinePoints(players[j].cards[players[j].counter],
+                players[j].score);
+            // udate indexes.
+            deckIndex++;
+            players[j].counter++;
+
+            // display p;ayer's cards and score.
+            cout << "   Cards: ";
+            for (int i = 0; i < players[j].counter; i++){
+              if (i == players[j].counter - 1){
+                displayCard(players[j].cards[i]);
+                cout << endl;
+              }
+              else{
+                displayCard(players[j].cards[i]);
+                cout << ", ";
+              }
+            }
+            cout<< "   Score: " << players[j].score << endl;
+
+            // check if player busted.
+            if (players[j].score >= 21)
+              players[j].isWithdrawn = true;
+            }
+        } while (players[j].isWithdrawn == false);
+      }
+    }
+
+    // display dealer's cards and score.
+    cout << "\nDealer:\n   Cards: ";
+    for (int i = 0; i < dealer.counter; i++){
+      if (i == dealer.counter - 1){
+        displayCard(dealer.cards[i]);
+        cout << endl;
+      }
+      else{
+        displayCard(dealer.cards[i]);
+        cout << ", ";
+      }
+    }
+    cout << "   Score: " << dealer.score << endl;
+    if (dealer.score < 21){
+      do{
+        // deal one card to the dealer.
+        dealer.cards[dealer.counter] = determineCardNumber(deck[deckIndex]);
+        // update dealer's score.
+        dealer.score += determinePoints(dealer.cards[dealer.counter],
+          dealer.score);
+        // update indexes.
+        deckIndex++;
+        dealer.counter++;
+
+        // display dealer's cards and score.
+        cout << "   Cards: ";
+        for (int i = 0; i < dealer.counter; i++){
+          if (i == dealer.counter - 1){
+            displayCard(dealer.cards[i]);
+            cout << endl;
+          }
+          else{
+            displayCard(dealer.cards[i]);
+            cout << ", ";
+          }
+
+        }
+        cout << "   Score: " << dealer.score << endl;
+
+        // check if dealer is over 16.
+        if (dealer.score >= 17)
+          dealer.isWithdrawn = true;
+      } while (dealer.isWithdrawn == false);
+    }
+
+    // Stage 5.
+    // display the results of the game.
+    for (int j = 0; j < numPlayers; j++){
+      if (players[j].score>21){ // player busts.
+        cout << "\nPlayer " << j + 1 << " BUSTED! Dealer wins!\n";
+        players[j].numChips -= 1;
+        dealer.numChips += 1;
+      }
+      else if (dealer.score > 21){ // dealer busts.
+        cout << "\nPlayer " << j + 1 << " wins! Dealer BUSTED!\n";
+        dealer.numChips -= 1;
+        players[j].numChips += 1;
+      }
+      else if (players[j].score < dealer.score){ // dealer has higher score.
+        cout << "\nPlayer " << j + 1 << " lost. Dealer wins!\n";
+        players[j].numChips -= 1;
+        dealer.numChips += 1;
+      }
+      else if (players[j].score > dealer.score){ // player has higer score.
+        cout << "\nPlayer " << j + 1 << " wins! Dealer lost.\n";
+        dealer.numChips -= 1;
+        players[j].numChips += 1;
+      }
+      else{ // dealer and player have same score.
+        cout << "\nPlayer " << j + 1 << " and Dealer push.\n";
+      }
+    }
+
+    // display final number of chips.
+    cout << "\nFinal Score:\n";
+    for (int i = 0; i < numPlayers; i++){
+      cout << "\n Player " << i + 1 << ":\n"
+        << "   Chips: " << players[i].numChips << " ($"
+        << players[i].numChips*VALUE_CHIP << ")\n";
+    }
+    cout << "\n Dealer:\n" << "   Chips: " << dealer.numChips << " ($"
+      << dealer.numChips*VALUE_CHIP << ")\n";
+
+    // ask user whether to play again.
+    cout << "\nPlay again? (yes/no): ";
+    cin >> playAgain;
+  } while (playAgain == "yes");
+
+  // Return 0 to the operating system.
+  return 0;
+}
+
+/****************************************************************
+ *                            swap                              *
+ * This function switch the values of two elements.             *
+ * Parameter: element1 - integer pointer to an array element.   *
+ *  element2 - integer pointer to an array element.             *
+ * Return : element1 - the new value of the first array element.*
+ ****************************************************************/
+int swap(int* element1, int* element2){
+  int temp = *element1; // Save first element into temp variable.
+  *element1 = *element2;
+  *element2 = temp;
+
+  // Return the new first element.
+  return *element1;
+}
+
+/****************************************************************
+ *                            shuffle                           *
+ * This function shuffle the elements of an integer array by    *
+ * swaping random elements of the array.                        *
+ * Parameter: ticket - an integer array.                        *
+ *  nSwap - number of times to swap elements.                   *
+ * Return: ticket[0] - first element of the shuffled array.     *
+ ****************************************************************/
+int shuffle(int ticket[], int nSwap){
+  // Declare local variables.
+  int card1;
+  int card2;
+
+  // Use current time as seed for rand function.
+  srand(time(NULL));
+
+  // Swap two random cards with each other.
+  for (int i = 0; i < NUM_SWAPS; i++){
+    // Pick two random cards.
+    card1 = rand() % (NUM_CARDS*NUM_SUITS*NUM_DECKS);
+    card2 = rand() % (NUM_CARDS*NUM_SUITS*NUM_DECKS);
+
+    // If the two cards are diffent, swap them.
+    if (card1 != card2)
+      swap(ticket[card1], ticket[card2]);
+  }
+
+  // Return the first card after shuffling.
+  return ticket[0];
+}
+
+/****************************************************************
+ *                            shufflePlayers                    *
+ * This function shuffle the elements of a person array by      *
+ * swaping random elements of the array.                        *
+ * Parameter: players - a person array.                         *
+ *  nSwap - number of times to swap elements.                   *
+ * Return: Nothing.                                             *
+ ****************************************************************/
+void shufflePlayers(person players[], int nSwap){
+  // Declare local variables.
+  int player1;
+  int player2;
+
+  // Swap two random players with each other.
+  for (int i = 0; i < NUM_SWAPS; i++){
+    // Pick two random players.
+    player1 = rand() % 7;
+    player2 = rand() % 7;
+
+    // If the two players are diffent, swap them.
+    if (player1 != player2)
+      swap(players[player1], players[player2]);
+  }
+}
+
+/****************************************************************
+ *                        determinePoints                       *
+ * This function determines points based on the ticketnumber    *
+ * provided.                                                    *
+ * Parameter: ticketNumber - the ticketNumber of the card.      *
+ *  score - the score of the player so far.                     *
+ * Return: the points for that particular card.                 *
+ ****************************************************************/
+int determinePoints(int cardNumber, int score){
+  if (cardNumber == 1){ // for aces.
+    if (score + 11 > 21) // check if the player will bust with score of 11.
+      return 1;
+    else
+      return 11;
+  }
+  else if (cardNumber > 1 && cardNumber < 11) // Number cards.
+    return cardNumber;
+  else // face cards.
+    return 10;
+}
+
+/****************************************************************
+ *                      determineCardNumber                     *
+ * This function determines points based on the ticketnumber    *
+ * provided.                                                    *
+ * Parameter: ticketNumber - the ticketNumber of the card.      *
+ * Return: the cardNumber of the card.                          *
+ ****************************************************************/
+int determineCardNumber(int ticketNumber){
+  return (ticketNumber % (NUM_SUITS*NUM_CARDS)) % NUM_CARDS + 1;
+}
+
+/****************************************************************
+ *                        displayCard                           *
+ * This function displays card info based on cardNumber.        *
+ * Parameter: cardNumber - the cardNumber of the card (1-13).   *
+ * Return: Nothing.                                             *
+ ****************************************************************/
+void displayCard(int cardNumber){
+  switch (cardNumber){
+    case 1: cout << "Ace";
+    break;
+    case 11: cout << "Jack";
+    break;
+    case 12: cout << "Queen";
+    break;
+    case 13: cout << "King";
+    break;
+    default: cout << cardNumber;
+  }
+}
